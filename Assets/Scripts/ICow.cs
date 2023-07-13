6418fa47fe590b41f60a8b7a46c6f15cfbf7d1b8
unknown <eiriak@uio.no>
2023-07-14 00:06:30 +0200

    Added Interface for the Cow so it's methods can be easily be called

---
+++ Assets/Scripts/ICow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICow
+{
+    public void TractorBeamed();
+
+    public void Walk();
+
+    public void Moo();
+}
