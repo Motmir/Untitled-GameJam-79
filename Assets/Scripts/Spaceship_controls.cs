801b68013bfc40ed7a258f5cb2547c1bd1e8397f
Matteo Brosstad Greco <auburnghost.tx@gmail.com>
2023-07-14 03:12:38 +0200

    Fixed up spaceship control and added gun visual

--- Assets/Scripts/Spaceship_controls.cs
+++ Assets/Scripts/Spaceship_controls.cs
@@ -6,42 +6,96 @@ using UnityEngine.InputSystem;
 
 public class Spaceship_controls : MonoBehaviour
 {
-
     //Attributes
-    public Rigidbody2D spaceshipRB;
+    public Camera cam;
+    public Rigidbody spaceshipRB;
+    public Transform shipbody;
     public GameObject beamObj;
-    public InputAction playerMove;
-    public InputAction beam;
+    public Transform gun;
 
+    //control
+    private Controls controls;
+    private Vector2 moveInput, mousePos;
+
+    
     //Variables
     public Vector2 acceleration = new Vector2(0.1f, 0.1f);
     public Vector2 maxSpeed = new Vector2(5, 5);
     public Vector2 drag = new Vector2(0.1f, 0.1f);
+
+    private void Awake()
+    {
+        controls = new Controls();
+        controls.Spaceship.Move.performed += Move;
+        controls.Spaceship.Aim.performed += Aim;
+        controls.Spaceship.Beam.performed += Beam;
+        controls.Spaceship.Shoot.performed += Shoot;
+    }
     void OnEnable()
     {
-        playerMove.Enable();
-        beam.Enable();
+        controls.Enable();
+        controls.Spaceship.Move.Enable();
+        controls.Spaceship.Aim.Enable();
+        controls.Spaceship.Beam.Enable();
+        controls.Spaceship.Shoot.Enable();
     }
     void OnDisable()
     {
-        playerMove.Disable();
-        beam.Disable();
+        controls.Disable();
+        controls.Spaceship.Move.Disable();
+        controls.Spaceship.Aim.Disable();
+        controls.Spaceship.Beam.Disable();
+        controls.Spaceship.Shoot.Disable();
     }
+
+
+    void Move(InputAction.CallbackContext c)
+    {
+        Debug.Log(moveInput);
+        moveInput = c.ReadValue<Vector2>();
+        
+    }
+
+    void Beam(InputAction.CallbackContext c)
+    {
+        bool beamOn = c.ReadValue<float>() > 0.5f;
+        beamObj.SetActive(beamOn);
+    }
+
+    void Aim(InputAction.CallbackContext c)
+    {
+        mousePos = c.ReadValue<Vector2>();
+        Debug.Log(cam.ScreenToWorldPoint(mousePos));
+        gun.up = (Vector2) cam.ScreenToWorldPoint(mousePos) - (Vector2) gun.position;
+    }
+
+    void Shoot(InputAction.CallbackContext c)
+    {
+        bool shootPressed = c.ReadValue<float>() > 0.5f;
+
+    }
+    
+
     void FixedUpdate()
     {
         //ship movement
-        Vector2 moveInput = playerMove.ReadValue<Vector2>();
-        Vector2 currentSpeed = spaceshipRB.velocity;
-        currentSpeed += moveInput * acceleration;
-        currentSpeed -= drag * currentSpeed;
-        if (Mathf.Abs(currentSpeed.x) > maxSpeed.x) { currentSpeed.x = maxSpeed.x * Mathf.Sign(currentSpeed.x); }
-        if (Mathf.Abs(currentSpeed.y) > maxSpeed.y) { currentSpeed.y = maxSpeed.y * Mathf.Sign(currentSpeed.y); }
-        spaceshipRB.velocity = currentSpeed;
-        Debug.Log(currentSpeed);
+        Vector2 currentVelocity = spaceshipRB.velocity;
+        Vector2 currentSpeed = new Vector2(Mathf.Abs(currentVelocity.x), Mathf.Abs(currentVelocity.y));
+        currentVelocity += moveInput * acceleration;
+        currentVelocity -= drag * currentVelocity;
+        if (currentSpeed.x > maxSpeed.x) { currentVelocity.x = maxSpeed.x * Mathf.Sign(currentVelocity.x); }
+        if (currentSpeed.y > maxSpeed.y) { currentVelocity.y = maxSpeed.y * Mathf.Sign(currentVelocity.y); }
+        spaceshipRB.velocity = currentVelocity;
+        Debug.Log(currentVelocity);
 
-        //beam
-        bool beamOn = beam.ReadValue<float>() > 0.5f;
-        beamObj.SetActive(beamOn);
+
+
+
+    //private float tilt = 0;
+    //public float maxTilt = 90f;
+    //tilt = Mathf.Lerp(tilt, Mathf.Sign(currentVelocity.x) * (speed) / (maxSpeed.x), 0.5f);
+    //shipbody.rotation = Quaternion.Euler(0, 0, -maxTilt * tilt);
+
     }
 }
 
