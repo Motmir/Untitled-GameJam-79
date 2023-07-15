505b746035636799c485cd6ae0d24afa72c49f69
LisBirUll <liseuliseu1@gmail.com>
2023-07-15 23:26:18 +0200

    First draft shop

---
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    public int coins;
+    public TMP_Text coinUI;
+    public ShopItem[] shopItems;
+    public ShopTemplate[] shopPanels;
+    public GameObject[] shopPanelsGO;
+    public Button[] myPurchaseBtns;
+    
+
+    void Start()
+    {
+        for (int i = 0; i < shopItems.Length; i++)
+        {
+            // Only the panles that has items are shown in runtime
+            shopPanelsGO[i].SetActive(true);
+        }
+
+        coinUI.text = "Coins: " + coins.ToString();
+        LoadPanels(); 
+        CheckPurchaseable();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // Press a button and get coins
+    public void AddCoins()
+    {
+        coins++;
+        coinUI.text = "Coins: " + coins.ToString();
+        CheckPurchaseable();
+    }
+
+    // Generate the panles with correct information
+    public void LoadPanels()
+    {
+        for (int i = 0; i < shopItems.Length; i++)
+        {
+            shopPanels[i].titleTxt.text = shopItems[i].title;
+            shopPanels[i].descriptionTxt.text = shopItems[i].description;
+            shopPanels[i].costTxt.text = "Coins: " + shopItems[i].baseCost.ToString();
+        }
+    }
+
+    public void CheckPurchaseable()
+    {
+        for(int i = 0;i < shopItems.Length; i++)
+        {
+            if (coins >= shopItems[i].baseCost) // if I have enough money
+            {
+                myPurchaseBtns[i].interactable = true;
+            } else
+            {
+                myPurchaseBtns[i].interactable = false;
+            }
+        }
+    }
+
+    public void PurchaseItem(int btnNo)
+    {
+        if (coins >= shopItems[btnNo].baseCost)
+        {
+            coins = coins - shopItems[btnNo].baseCost;
+            coinUI.text = "Coins: " + coins.ToString();
+            CheckPurchaseable();
+            // Unlock Item
+            // TODO: Add this functionality
+        }
+    }
+ 
+
+
+}
