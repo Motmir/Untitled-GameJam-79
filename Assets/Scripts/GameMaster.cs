971d03a1c6baa0574c3719b2c5904a3372aed057
AuburnGhost <auburnghost.tx@gmail.com>
2023-07-15 21:05:36 +0200

    Wobble on asteroid

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMaster : MonoBehaviour
 {
     public static GameMaster Instance;
     public int cownter = 0;
+    private int scene = 0;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -27,6 +30,22 @@ public class GameMaster : MonoBehaviour
     {
         cownter--;
         GameObject.Find("Cownter").GetComponent<TextMeshProUGUI>().text = "Cows: " + GameObject.Find("GameMaster").GetComponent<GameMaster>().cownter;
+    }
+
+    public void SwichScenes()
+    {
+        scene = (scene + 1) % SceneManager.sceneCountInBuildSettings;
+
+        if(scene == 0)
+        {
+            SceneManager.LoadScene("Earth Scene");
+        } else if (scene == 1)
+        {
+            SceneManager.LoadScene("Farm Scene");
+        } else if (scene == 2)
+        {
+            SceneManager.LoadScene("Space");
+        }
     }
 
 
