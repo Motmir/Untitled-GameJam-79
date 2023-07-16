a753601f029d870944e0f4219ae0a2347f17e1a7
LisBirUll <liseuliseu1@gmail.com>
2023-07-16 17:31:41 +0200

    Soundssss!

--- Assets/Scripts/Shop/ShopTemplate.cs
+++ Assets/Scripts/Shop/ShopTemplate.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopTemplate : MonoBehaviour
 {
     public TMP_Text titleTxt;
     public TMP_Text descriptionTxt;
     public TMP_Text costTxt;
+    public Image artwork;
 }
