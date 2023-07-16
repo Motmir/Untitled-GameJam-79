fd4941e15d81c82ad1c6a4d850c040fd90d4d63c
LisBirUll <liseuliseu1@gmail.com>
2023-07-16 18:37:56 +0200

    First draft title screen

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController: MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
