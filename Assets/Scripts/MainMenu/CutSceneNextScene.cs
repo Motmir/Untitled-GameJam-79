94179291f11d7709e91fcaa057673c5c66092c0a
LisBirUll <liseuliseu1@gmail.com>
2023-07-16 20:08:08 +0200

    cutscene begining

---
+++ Assets/Scripts/MainMenu/CutSceneNextScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutSceneNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(NextScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(10f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+ }
